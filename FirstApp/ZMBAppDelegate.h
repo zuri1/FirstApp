a029995887fe921393575ad36dd033d99fc50596
zurizurizuri <zuribiringer@Zuri-Biringers-MacBook-Pro.local>
2013-10-17 19:56:14 -0700

    Initial Commit

---
+++ FirstApp/ZMBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZMBAppDelegate.h
+//  FirstApp
+//
+//  Created by Zuri Biringer on 10/17/13.
+//  Copyright (c) 2013 Zuri Biringer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZMBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
